70f16345b3a2bb08a7082eedf0c9ffb70d2191d2
Avdyl <avdyl.zekaj@dci-student.org>
2025-05-15 15:30:51 +0200

    Endpont & ServerRoutes

--- routers/chatbot.js
+++ routers/chatbot.js
@@ -18,7 +18,7 @@ router.post("/", async (req, res) => {
     const response = await fetch("https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta", {
         method: "POST",
         headers: {
-            Authorization: "Bearer" + process.env.CHATBOT_API_KEY,
+            Authorization: "Bearer " + process.env.CHATBOT_API_KEY,
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
